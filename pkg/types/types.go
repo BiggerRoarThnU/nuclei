23f3957271959adf09d30442ff2643da0c642729
Dwi Siswanto <git@dw1.io>
2026-01-03 09:28:53 +0700

    fix(types): add `DisableHTTPCache` to `Copy` method
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -559,6 +559,7 @@ func (options *Options) Copy() *Options {
 		DebugRequests:                  options.DebugRequests,
 		DebugResponse:                  options.DebugResponse,
 		DisableHTTPProbe:               options.DisableHTTPProbe,
+		DisableHTTPCache:               options.DisableHTTPCache,
 		LeaveDefaultPorts:              options.LeaveDefaultPorts,
 		AutomaticScan:                  options.AutomaticScan,
 		Silent:                         options.Silent,
